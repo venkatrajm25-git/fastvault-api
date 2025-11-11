aca9b83ff384b1f4b62979725c5208e42eb14ce4
Venkatraj <venkatrajm25@gmail.com>
2025-11-12 01:22:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,3 +20,11 @@ register_exception_handlers(app)
 @app.get("/")
 async def home():
     return JSONResponse(content={"success": True, "message": "FastVaultAPI is working"})
+
+
+import os
+import uvicorn
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))  # Render gives this env variable
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
